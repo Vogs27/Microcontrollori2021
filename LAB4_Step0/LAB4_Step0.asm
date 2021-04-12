bd682e675194a8015245357b6c48ab18c5341ebd
AleVog <Vogs27@users.noreply.github.com>
2021-04-12 19:20:18 +0200

    Step0

--- LAB4_Step0/LAB4_Step0.asm
+++ LAB4_Step0/LAB4_Step0.asm
@@ -1,118 +1,129 @@
 
 _main:
 
-;LAB4_Step0.c,6 :: 		void main() {
-;LAB4_Step0.c,7 :: 		T0CON = 0b11000111;
+;LAB4_Step0.c,20 :: 		void main() {
+;LAB4_Step0.c,21 :: 		char pattern[5]={0b11111111, 0b01111110, 0b00111100, 0b00011000, 0b00000000};
+	MOVLW       255
+	MOVWF       main_pattern_L0+0 
+	MOVLW       126
+	MOVWF       main_pattern_L0+1 
+	MOVLW       60
+	MOVWF       main_pattern_L0+2 
+	MOVLW       24
+	MOVWF       main_pattern_L0+3 
+	CLRF        main_pattern_L0+4 
+	MOVLW       1
+	MOVWF       main_dir_L0+0 
+	CLRF        main_cycle_L0+0 
+;LAB4_Step0.c,26 :: 		T0CON = 0b11000111;
 	MOVLW       199
 	MOVWF       T0CON+0 
-;LAB4_Step0.c,8 :: 		TMR0L = 248;
+;LAB4_Step0.c,27 :: 		TMR0L = 248;
 	MOVLW       248
 	MOVWF       TMR0L+0 
-;LAB4_Step0.c,9 :: 		TRISD = 0b00000000;
+;LAB4_Step0.c,28 :: 		TRISD = 0b00000000;
 	CLRF        TRISD+0 
-;LAB4_Step0.c,13 :: 		INTCON.TMR0IE = 1;
+;LAB4_Step0.c,31 :: 		INTCON.TMR0IE = 1;
 	BSF         INTCON+0, 5 
-;LAB4_Step0.c,14 :: 		INTCON.GIE = 1;
+;LAB4_Step0.c,32 :: 		INTCON.GIE = 1;
 	BSF         INTCON+0, 7 
-;LAB4_Step0.c,16 :: 		while(1){
+;LAB4_Step0.c,35 :: 		Lcd_Cmd(_LCD_CLEAR);               // Clear display
+	MOVLW       1
+	MOVWF       FARG_Lcd_Cmd_out_char+0 
+	CALL        _Lcd_Cmd+0, 0
+;LAB4_Step0.c,36 :: 		Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
+	MOVLW       12
+	MOVWF       FARG_Lcd_Cmd_out_char+0 
+	CALL        _Lcd_Cmd+0, 0
+;LAB4_Step0.c,38 :: 		while(1){
 L_main0:
-;LAB4_Step0.c,17 :: 		LATD = pattern[cycle];
-	MOVLW       _pattern+0
+;LAB4_Step0.c,40 :: 		if(counter_kitt > 1000){
+	MOVF        _counter_kitt+1, 0 
+	SUBLW       3
+	BTFSS       STATUS+0, 2 
+	GOTO        L__main9
+	MOVF        _counter_kitt+0, 0 
+	SUBLW       232
+L__main9:
+	BTFSC       STATUS+0, 0 
+	GOTO        L_main2
+;LAB4_Step0.c,41 :: 		counter_kitt = 0;
+	CLRF        _counter_kitt+0 
+	CLRF        _counter_kitt+1 
+;LAB4_Step0.c,42 :: 		LATD = pattern[cycle];
+	MOVLW       main_pattern_L0+0
 	MOVWF       FSR0L+0 
-	MOVLW       hi_addr(_pattern+0)
+	MOVLW       hi_addr(main_pattern_L0+0)
 	MOVWF       FSR0L+1 
-	MOVF        _cycle+0, 0 
+	MOVF        main_cycle_L0+0, 0 
 	ADDWF       FSR0L+0, 1 
-	MOVLW       0
-	BTFSC       _cycle+0, 7 
-	MOVLW       255
-	ADDWFC      FSR0L+1, 1 
+	BTFSC       STATUS+0, 0 
+	INCF        FSR0L+1, 1 
 	MOVF        POSTINC0+0, 0 
 	MOVWF       LATD+0 
-;LAB4_Step0.c,18 :: 		if (cycle == 4)
-	MOVF        _cycle+0, 0 
+;LAB4_Step0.c,43 :: 		if (cycle == 4){
+	MOVF        main_cycle_L0+0, 0 
 	XORLW       4
 	BTFSS       STATUS+0, 2 
-	GOTO        L_main2
-;LAB4_Step0.c,19 :: 		dir = -1;
-	MOVLW       255
-	MOVWF       _dir+0 
-	MOVLW       255
-	MOVWF       _dir+1 
-L_main2:
-;LAB4_Step0.c,20 :: 		if (cycle == 0)
-	MOVF        _cycle+0, 0 
+	GOTO        L_main3
+;LAB4_Step0.c,44 :: 		dir = 0;
+	CLRF        main_dir_L0+0 
+;LAB4_Step0.c,45 :: 		}
+L_main3:
+;LAB4_Step0.c,46 :: 		if (cycle == 0){
+	MOVF        main_cycle_L0+0, 0 
 	XORLW       0
 	BTFSS       STATUS+0, 2 
-	GOTO        L_main3
-;LAB4_Step0.c,21 :: 		dir = 1;
+	GOTO        L_main4
+;LAB4_Step0.c,47 :: 		dir = 1;
 	MOVLW       1
-	MOVWF       _dir+0 
-	MOVLW       0
-	MOVWF       _dir+1 
-L_main3:
-;LAB4_Step0.c,22 :: 		}
+	MOVWF       main_dir_L0+0 
+;LAB4_Step0.c,48 :: 		}
+L_main4:
+;LAB4_Step0.c,49 :: 		if (dir == 1){
+	MOVF        main_dir_L0+0, 0 
+	XORLW       1
+	BTFSS       STATUS+0, 2 
+	GOTO        L_main5
+;LAB4_Step0.c,50 :: 		cycle ++;
+	INCF        main_cycle_L0+0, 1 
+;LAB4_Step0.c,51 :: 		}
+	GOTO        L_main6
+L_main5:
+;LAB4_Step0.c,53 :: 		cycle --;
+	DECF        main_cycle_L0+0, 1 
+;LAB4_Step0.c,54 :: 		}
+L_main6:
+;LAB4_Step0.c,55 :: 		}
+L_main2:
+;LAB4_Step0.c,66 :: 		}
 	GOTO        L_main0
-;LAB4_Step0.c,23 :: 		}
+;LAB4_Step0.c,67 :: 		}
 L_end_main:
 	GOTO        $+0
 ; end of _main
 
 _interrupt:
 
-;LAB4_Step0.c,25 :: 		void interrupt(){
-;LAB4_Step0.c,26 :: 		if(INTCON.TMR0IF){
+;LAB4_Step0.c,69 :: 		void interrupt(){
+;LAB4_Step0.c,70 :: 		if(INTCON.TMR0IF){
 	BTFSS       INTCON+0, 2 
-	GOTO        L_interrupt4
-;LAB4_Step0.c,27 :: 		INTCON.TMR0IF=0;
+	GOTO        L_interrupt7
+;LAB4_Step0.c,71 :: 		INTCON.TMR0IF=0;
 	BCF         INTCON+0, 2 
-;LAB4_Step0.c,28 :: 		TMR0L = 248;
+;LAB4_Step0.c,72 :: 		TMR0L = 248;
 	MOVLW       248
 	MOVWF       TMR0L+0 
-;LAB4_Step0.c,29 :: 		counter_ms++;
+;LAB4_Step0.c,73 :: 		counter_ms++;
 	INFSNZ      _counter_ms+0, 1 
 	INCF        _counter_ms+1, 1 
-;LAB4_Step0.c,30 :: 		if(counter_ms>=1000){
-	MOVLW       3
-	SUBWF       _counter_ms+1, 0 
-	BTFSS       STATUS+0, 2 
-	GOTO        L__interrupt11
-	MOVLW       232
-	SUBWF       _counter_ms+0, 0 
-L__interrupt11:
-	BTFSS       STATUS+0, 0 
-	GOTO        L_interrupt5
-;LAB4_Step0.c,31 :: 		counter_sec++;
-	INFSNZ      _counter_sec+0, 1 
-	INCF        _counter_sec+1, 1 
-;LAB4_Step0.c,32 :: 		counter_ms=0;
-	CLRF        _counter_ms+0 
-	CLRF        _counter_ms+1 
-;LAB4_Step0.c,33 :: 		if (dir == 1){
-	MOVLW       0
-	XORWF       _dir+1, 0 
-	BTFSS       STATUS+0, 2 
-	GOTO        L__interrupt12
-	MOVLW       1
-	XORWF       _dir+0, 0 
-L__interrupt12:
-	BTFSS       STATUS+0, 2 
-	GOTO        L_interrupt6
-;LAB4_Step0.c,34 :: 		cycle ++;
-	INCF        _cycle+0, 1 
-;LAB4_Step0.c,35 :: 		}
-	GOTO        L_interrupt7
-L_interrupt6:
-;LAB4_Step0.c,37 :: 		cycle --;
-	DECF        _cycle+0, 1 
-;LAB4_Step0.c,38 :: 		}
+;LAB4_Step0.c,74 :: 		counter_kitt++;
+	INFSNZ      _counter_kitt+0, 1 
+	INCF        _counter_kitt+1, 1 
+;LAB4_Step0.c,75 :: 		}
 L_interrupt7:
-;LAB4_Step0.c,39 :: 		}
-L_interrupt5:
-;LAB4_Step0.c,40 :: 		}
-L_interrupt4:
-;LAB4_Step0.c,41 :: 		}
+;LAB4_Step0.c,76 :: 		}
 L_end_interrupt:
-L__interrupt10:
+L__interrupt11:
 	RETFIE      1
 ; end of _interrupt
--- LAB4_Step0/LAB4_Step0.c
+++ LAB4_Step0/LAB4_Step0.c
@@ -1,25 +1,69 @@
 unsigned int counter_ms = 0;
-unsigned int counter_sec = 0;
-char pattern[5]={0b11111111, 0b01111110, 0b00111100, 0b00011000, 0b00000000};
-int dir = 1;
-short cycle = 0;
+unsigned int counter_kitt = 0;
+
+//LCD module
+sbit LCD_RS at LATB4_bit;
+sbit LCD_EN at LATB5_bit;
+sbit LCD_D4 at LATB0_bit;
+sbit LCD_D5 at LATB1_bit;
+sbit LCD_D6 at LATB2_bit;
+sbit LCD_D7 at LATB3_bit;
+
+sbit LCD_RS_Direction at TRISB4_bit;
+sbit LCD_EN_Direction at TRISB5_bit;
+sbit LCD_D4_Direction at TRISB0_bit;
+sbit LCD_D5_Direction at TRISB1_bit;
+sbit LCD_D6_Direction at TRISB2_bit;
+sbit LCD_D7_Direction at TRISB3_bit;
+
+
 void main() {
+    char pattern[5]={0b11111111, 0b01111110, 0b00111100, 0b00011000, 0b00000000};
+    char dir = 1;
+    char cycle = 0;
+    char counter_sec = 0;
+    char output_num[3];
     T0CON = 0b11000111;
     TMR0L = 248;
     TRISD = 0b00000000;
-    //ANSELD = 0b00000000;
     //IOCD non serve, ritardo fisso
     //Interrupt
     INTCON.TMR0IE = 1;
     INTCON.GIE = 1;
+    
+    //Lcd_Init();                        // Initialize Lcd
+    Lcd_Cmd(_LCD_CLEAR);               // Clear display
+    Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
 
     while(1){
-        LATD = pattern[cycle];
-        if (cycle == 4)
-             dir = -1;
-        if (cycle == 0)
-             dir = 1;
-    }
+            //KIT SECTION
+            if(counter_kitt > 1000){
+                counter_kitt = 0;
+                LATD = pattern[cycle];
+                if (cycle == 4){
+                    dir = 0;
+                }
+                if (cycle == 0){
+                    dir = 1;
+                }
+                if (dir == 1){
+                    cycle ++;
+                }
+                else {
+                    cycle --;
+                }
+            }
+
+            //COUNTER AND DISPLAY SECTION
+           /* if(counter_ms > 500){
+                counter_sec++;
+                counter_ms=0;
+                Lcd_Cmd(_LCD_CLEAR);
+                Lcd_Cmd(_LCD_CURSOR_OFF);
+                IntToStr(counter_sec,output_num);
+                Lcd_Out(1,1,output_num);
+            }*/
+        }
 }
 
 void interrupt(){
@@ -27,18 +71,10 @@ void interrupt(){
         INTCON.TMR0IF=0;
         TMR0L = 248;
         counter_ms++;
-        if(counter_ms>=1000){
-            counter_sec++;
-            counter_ms=0; 
-        if (dir == 1){
-        cycle ++;
-        }
-        else {
-        cycle --;
-        }
-        }
+        counter_kitt++;
     }
 }
+
 /*
 short cycle = 0;
 char portOut = 0b11111111 & (2^cycle); //populating the first nibble
@@ -61,6 +97,4 @@ portOut = portOut & (portOut
 2^1 = 10 
 2^2 = 100
 2^3 = 1000
-
-
 */
